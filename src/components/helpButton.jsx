4d70c24bb46df197cc5eb9dd798c5909f67aa493
Anon2148 <privext@tutanota.com>
2024-05-04 20:10:18 +0200

    feat: added modal panel when clicking help button

--- src/components/helpButton.jsx
+++ src/components/helpButton.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import './styles/helpButton.css';
-import HelpIcon from '@mui/icons-material/Help';
+import React from "react";
+import { useState } from "react";
+import "./styles/helpButton.css";
+import HelpIcon from "@mui/icons-material/Help";
+import LogoImage from "../images/logo.png";
+import "./subcomponents/styles/modal.css";
 
-function HelpButton()
-{
-    return (
-        <div id="helpButtonContainer">
-            <HelpIcon id="helpButton"/>
-        </div>
-    );
+const HelpMenu = ({ active }) => {
+  if (!active) return null;
+
+  return (
+    <>
+      <div className="modal">
+        <section className="modal-main">
+          <img src={LogoImage} alt="Help 1" />
+          <p>Texto imagen 1</p>
+        </section>
+      </div>
+    </>
+  );
+};
+
+function HelpButton() {
+  const [enabled, setEnabled] = useState(false);
+
+  const handleClick = () => {
+    setEnabled(!enabled);
+  };
+  return (
+    <>
+      <div id="helpButtonContainer" onClick={handleClick}>
+        <HelpIcon id="helpButton" />
+      </div>
+      <HelpMenu active={enabled} />
+    </>
+  );
 }
 
 export default HelpButton;
