47764f4e2cfeb13c24d14e40dc24c61454cb494c
Anon2148 <privext@tutanota.com>
2024-05-10 00:28:39 +0200

    "feat: added new functions in intercambio.jsx to display the text, the input and the button, also added some css styles"

--- src/components/subcomponents/gameState.jsx
+++ src/components/subcomponents/gameState.jsx
@@ -1,65 +1,57 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Ruleta from './sub-gameStatus/ruleta';
-import Almacen from './sub-gameStatus/almacen';
-import Marcadores from './sub-gameStatus/marcadores'
-import Pokedex from './sub-gameStatus/pokedex';
-import VerPokemonAlmacen from './sub-gameStatus/verPokemonAlmacen';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Ruleta from "./sub-gameStatus/ruleta";
+import Almacen from "./sub-gameStatus/almacen";
+import Marcadores from "./sub-gameStatus/marcadores";
+import Pokedex from "./sub-gameStatus/pokedex";
+import VerPokemonAlmacen from "./sub-gameStatus/verPokemonAlmacen";
+import Intercambio from "./sub-gameStatus/intercambio";
 
 function GameState() {
-    return (
-        <Routes>
-            <Route path='/ruleta' element={<RuletaGameState/>} />
+  return (
+    <Routes>
+      <Route path="/ruleta" element={<RuletaGameState />} />
 
-            <Route path='/almacen' element={<AlmacenGameState/>} />
+      <Route path="/almacen" element={<AlmacenGameState />} />
 
-            <Route path='/almacen/ver-pokemon' element={<VerPokemonAlmacenGameState/>} />
+      <Route
+        path="/almacen/ver-pokemon"
+        element={<VerPokemonAlmacenGameState />}
+      />
 
-            <Route path='/pokedex' element={<PokedexGameState/>} />
+      <Route path="/pokedex" element={<PokedexGameState />} />
 
-            <Route path='/marcadores' element={<MarcadoresGameState/>} />
+      <Route path="/marcadores" element={<MarcadoresGameState />} />
 
-            <Route path='/intercambio' element={<IntercambioGameState/>} />
+      <Route path="/intercambio" element={<IntercambioGameState />} />
 
-            <Route path='*' element={<></>} />
-        </Routes>
-    );
+      <Route path="*" element={<></>} />
+    </Routes>
+  );
 }
 
 function RuletaGameState() {
-    return (
-        <Ruleta />
-    );
+  return <Ruleta />;
 }
 
 function AlmacenGameState() {
-    return (
-        <Almacen />
-    );
+  return <Almacen />;
 }
 
 function VerPokemonAlmacenGameState() {
-    return (
-        <VerPokemonAlmacen />
-    );
+  return <VerPokemonAlmacen />;
 }
 
 function PokedexGameState() {
-    return (
-        <Pokedex />
-    );
+  return <Pokedex />;
 }
 
 function MarcadoresGameState() {
-    return (
-        <Marcadores />
-    );
+  return <Marcadores />;
 }
 
 function IntercambioGameState() {
-    
+  return <Intercambio />;
 }
-
-
 
 export default GameState;
--- src/components/subcomponents/sub-gameStatus/intercambio.jsx
+++ src/components/subcomponents/sub-gameStatus/intercambio.jsx
@@ -1,3 +1,40 @@
-function Intercambio {
-    
+import "./styles/intercambio.css";
+import "../../styles/panel.css";
+
+export default function Intercambio() {
+  return (
+    <>
+      <div id="gamestatePanelContainer">
+        <table>
+          <tbody className="primerVistaInicioIntercambioBody">
+            <tr>
+              <TextoInformativo />
+            </tr>
+            <tr>
+              <InputCodigoIntercambio />
+            </tr>
+            <tr>
+              <BotonIntercambio />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+function TextoInformativo() {
+  return (
+    <h1 className="intercambioTextoInicio">
+      Introduce el código de intercambio
+    </h1>
+  );
+}
+
+function InputCodigoIntercambio() {
+  return <input className="intercambioInputCodigo"></input>;
+}
+
+function BotonIntercambio() {
+  return <button className="intercambioBoton">Buscar</button>;
 }
