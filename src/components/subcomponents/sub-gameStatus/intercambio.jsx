0633c4fb4cb3c92de047621b15a9c2745bf22457
Anon2148 <privext@tutanota.com>
2024-05-14 01:09:22 +0200

    misc: changed url parameter to be more precise

--- src/components/subcomponents/sub-gameStatus/intercambio.jsx
+++ src/components/subcomponents/sub-gameStatus/intercambio.jsx
@@ -4,14 +4,13 @@ import { Link } from "react-router-dom";
 export default function Intercambio() {
   return (
     <>
-      
       <div className="botonesIntercambio">
-        <Link to="tipo?code=no">
+        <Link to="tipo?hasCode=no">
           <button className="intercambioBotonSinCodigo">
             <h2>No tengo código de intercambio</h2>
           </button>
         </Link>
-        <Link to="tipo?code=yes">
+        <Link to="tipo?hasCode=yes">
           <button className="intercambioBotonConCodigo">
             <h2>Tengo código de intercambio</h2>
           </button>
