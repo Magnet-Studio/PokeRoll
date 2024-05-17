84336ba565842f452d206c1c392fa84473032f80
Anon2148 <privext@tutanota.com>
2024-05-17 21:27:41 +0200

    feat: added new file to make the view of a exchange simulation, added the route for redirection in gameState

--- src/components/subcomponents/gameState.jsx
+++ src/components/subcomponents/gameState.jsx
@@ -8,29 +8,84 @@ import VerPokemonAlmacen from "./sub-gameStatus/verPokemonAlmacen";
 import TipoIntercambio from "./sub-gameStatus/tipointercambio";
 import Intercambio from "./sub-gameStatus/intercambio";
 import PantallaCargaIntercambio from "./sub-gameStatus/pantallaCargaIntercambio";
+import IntercambioSimulado from "./sub-gameStatus/intercambioSimulado";
+import { PlayerList } from "./sub-gameStatus/userdata/rankingList";
 
-function GameState({UserData, threePokemon, tirarButtonDisable, TierRuleta, setThreePokemon, setTirarButtonDisable, setChangeTierButtonDisable, setUserData}) {
+function GameState({
+  UserData,
+  threePokemon,
+  tirarButtonDisable,
+  TierRuleta,
+  setThreePokemon,
+  setTirarButtonDisable,
+  setChangeTierButtonDisable,
+  setUserData,
+}) {
   return (
     <Routes>
+      <Route
+        path="/"
+        element={
+          <RuletaGameState
+            threePokemon={threePokemon}
+            tirarButtonDisable={tirarButtonDisable}
+            TierRuleta={TierRuleta}
+            setThreePokemon={setThreePokemon}
+            setTirarButtonDisable={setTirarButtonDisable}
+            setChangeTierButtonDisable={setChangeTierButtonDisable}
+            setUserData={setUserData}
+          />
+        }
+      />
 
-      <Route path="/" element={<RuletaGameState threePokemon={threePokemon} tirarButtonDisable={tirarButtonDisable} TierRuleta={TierRuleta} setThreePokemon={setThreePokemon} setTirarButtonDisable={setTirarButtonDisable} setChangeTierButtonDisable={setChangeTierButtonDisable} setUserData={setUserData}/>} />
+      <Route
+        path="/ruleta"
+        element={
+          <RuletaGameState
+            UserData={UserData}
+            threePokemon={threePokemon}
+            tirarButtonDisable={tirarButtonDisable}
+            TierRuleta={TierRuleta}
+            setThreePokemon={setThreePokemon}
+            setTirarButtonDisable={setTirarButtonDisable}
+            setChangeTierButtonDisable={setChangeTierButtonDisable}
+            setUserData={setUserData}
+          />
+        }
+      />
 
-      <Route path="/ruleta" element={<RuletaGameState UserData={UserData} threePokemon={threePokemon} tirarButtonDisable={tirarButtonDisable} TierRuleta={TierRuleta} setThreePokemon={setThreePokemon} setTirarButtonDisable={setTirarButtonDisable} setChangeTierButtonDisable={setChangeTierButtonDisable} setUserData={setUserData}/>} />
-
-      <Route path="/almacen" element={<AlmacenGameState UserData={UserData} setUserData={setUserData}/>} />
+      <Route
+        path="/almacen"
+        element={
+          <AlmacenGameState UserData={UserData} setUserData={setUserData} />
+        }
+      />
 
       <Route
         path="/almacen/ver-pokemon"
-        element={<VerPokemonAlmacenGameState UserData={UserData} setUserData={setUserData} />}
+        element={
+          <VerPokemonAlmacenGameState
+            UserData={UserData}
+            setUserData={setUserData}
+          />
+        }
       />
 
-      <Route path="/pokedex" element={<PokedexGameState UserData={UserData}/>} />
+      <Route
+        path="/pokedex"
+        element={<PokedexGameState UserData={UserData} />}
+      />
 
       <Route path="/marcadores" element={<MarcadoresGameState />} />
 
       <Route path="/intercambio/tipo" element={<TipoIntercambioGameState />} />
 
       <Route path="/intercambio" element={<IntercambioGameState />} />
+
+      <Route
+        path="/intercambio/conexion"
+        element={<ConexionIntercambioGameState />}
+      />
 
       <Route
         path="/intercambio/pantallaCarga"
@@ -42,20 +97,40 @@ function GameState({UserData, threePokemon, tirarButtonDisable, TierRuleta, setT
   );
 }
 
-function RuletaGameState({UserData, threePokemon, tirarButtonDisable, TierRuleta, setThreePokemon, setTirarButtonDisable, setChangeTierButtonDisable, setUserData}) {
-  return <Ruleta UserData={UserData} threePokemon={threePokemon} tirarButtonDisable={tirarButtonDisable} TierRuleta={TierRuleta} setThreePokemon={setThreePokemon} setTirarButtonDisable={setTirarButtonDisable} setChangeTierButtonDisable={setChangeTierButtonDisable} setUserData={setUserData}/>;
+function RuletaGameState({
+  UserData,
+  threePokemon,
+  tirarButtonDisable,
+  TierRuleta,
+  setThreePokemon,
+  setTirarButtonDisable,
+  setChangeTierButtonDisable,
+  setUserData,
+}) {
+  return (
+    <Ruleta
+      UserData={UserData}
+      threePokemon={threePokemon}
+      tirarButtonDisable={tirarButtonDisable}
+      TierRuleta={TierRuleta}
+      setThreePokemon={setThreePokemon}
+      setTirarButtonDisable={setTirarButtonDisable}
+      setChangeTierButtonDisable={setChangeTierButtonDisable}
+      setUserData={setUserData}
+    />
+  );
 }
 
-function AlmacenGameState({UserData, setUserData}) {
-  return <Almacen UserData={UserData} setUserData={setUserData}/>;
+function AlmacenGameState({ UserData, setUserData }) {
+  return <Almacen UserData={UserData} setUserData={setUserData} />;
 }
 
-function VerPokemonAlmacenGameState({UserData, setUserData}) {
+function VerPokemonAlmacenGameState({ UserData, setUserData }) {
   return <VerPokemonAlmacen UserData={UserData} setUserData={setUserData} />;
 }
 
-function PokedexGameState({UserData}) {
-  return <Pokedex UserData={UserData}/>;
+function PokedexGameState({ UserData }) {
+  return <Pokedex UserData={UserData} />;
 }
 
 function MarcadoresGameState() {
@@ -72,6 +147,12 @@ function IntercambioGameState() {
 
 function PantallaCargaGameState() {
   return <PantallaCargaIntercambio />;
+}
+
+function ConexionIntercambioGameState() {
+  const exchangePlayerIndex = Math.floor(Math.random() * PlayerList.length);
+  const exchangeUserName = PlayerList[exchangePlayerIndex].playername;
+  return <IntercambioSimulado userToExchange={exchangeUserName} />;
 }
 
 export default GameState;
