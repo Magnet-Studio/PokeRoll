0cacb2100fa91619db283dcd08872758701b2458
Anon2148 <privext@tutanota.com>
2024-06-05 14:26:54 +0200

    feat: added new feature when no pokemon founded in almacen, you are not allowed to exchange any pokemon

--- src/components/subcomponents/sub-gameStatus/intercambio.jsx
+++ src/components/subcomponents/sub-gameStatus/intercambio.jsx
@@ -2,20 +2,29 @@ import { useEffect } from "react";
 import "./styles/intercambio.css";
 import { Link } from "react-router-dom";
 
-export default function Intercambio() {
+export default function Intercambio({ UserData }) {
   useEffect(() => {
     document.title = "PokéROLL (Intercambio)"
   }, [])
+
+  const almacenPokemonLength = [...UserData.pokemonList].length;
+  const noIntercambioMessage = (almacenPokemonLength === 1) ? 
+    <div className="mensajeNoIntercambioPokemon">
+      <p>No tienes pokemon para intercambiar</p>
+    </div>
+    :
+    <></>;
   return (
     <>
+      {noIntercambioMessage}
       <div className="botonesIntercambio">
-        <Link to="tipo?hasCode=no">
-          <button className="intercambioBotonSinCodigo">
+        <Link to="tipo?hasCode=no" className={almacenPokemonLength === 1 ? "inactiveLink" : ""}>
+          <button className={"intercambioBotonSinCodigo " + (almacenPokemonLength === 1 ? "disabled" : "enabled")}>
             <h2>No tengo código de intercambio</h2>
           </button>
         </Link>
-        <Link to="tipo?hasCode=yes">
-          <button className="intercambioBotonConCodigo">
+        <Link to="tipo?hasCode=yes" className={almacenPokemonLength === 1 ? "inactiveLink" : ""}>
+          <button className={"intercambioBotonConCodigo " + (almacenPokemonLength === 1 ? "disabled" : "enabled")}>
             <h2>Tengo código de intercambio</h2>
           </button>
         </Link>
