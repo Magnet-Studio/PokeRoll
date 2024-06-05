56e4ec4cc3715d4c6115e267ca2d032758ac9193
Anon2148 <privext@tutanota.com>
2024-06-05 16:04:34 +0200

    fix: fixed some color contrastproblems for accesibility

--- src/components/subcomponents/sub-gameStatus/intercambio.jsx
+++ src/components/subcomponents/sub-gameStatus/intercambio.jsx
@@ -4,29 +4,46 @@ import { Link } from "react-router-dom";
 
 export default function Intercambio({ UserData }) {
   useEffect(() => {
-    document.title = "PokéROLL (Intercambio)"
-  }, [])
+    document.title = "PokéROLL (Intercambio)";
+  }, []);
 
   const almacenPokemonLength = [...UserData.pokemonList].length;
-  const noIntercambioMessage = (almacenPokemonLength === 1) ? 
-    <div className="mensajeNoIntercambioPokemon">
-      <p>No tienes pokemon para intercambiar</p>
-    </div>
-    :
-    <></>;
+  const noIntercambioMessage =
+    almacenPokemonLength === 1 ? (
+      <div
+        className="mensajeNoIntercambioPokemon"
+        tabIndex="0"
+        aria-label="No tienes pokemon para intercambiar"
+      >
+        <p>No tienes pokemon para intercambiar</p>
+      </div>
+    ) : (
+      <></>
+    );
+  const linkClass = almacenPokemonLength === 1 ? "disabled" : "enabled";
   return (
     <>
       {noIntercambioMessage}
       <div className="botonesIntercambio">
-        <Link to="tipo?hasCode=no" className={almacenPokemonLength === 1 ? "inactiveLink" : ""}>
-          <button className={"intercambioBotonSinCodigo " + (almacenPokemonLength === 1 ? "disabled" : "enabled")}>
-            <h2>No tengo código de intercambio</h2>
-          </button>
+        <Link
+          to="tipo?hasCode=no"
+          className={linkClass + " intercambioBotonSinCodigo"}
+          tabIndex={linkClass === "disabled" ? "-1" : "0"}
+          aria-label={
+            linkClass === "enabled" ? "Botón para intercambiar sin código" : ""
+          }
+        >
+          <h2>No tengo código de intercambio</h2>
         </Link>
-        <Link to="tipo?hasCode=yes" className={almacenPokemonLength === 1 ? "inactiveLink" : ""}>
-          <button className={"intercambioBotonConCodigo " + (almacenPokemonLength === 1 ? "disabled" : "enabled")}>
-            <h2>Tengo código de intercambio</h2>
-          </button>
+        <Link
+          to="tipo?hasCode=yes"
+          className={linkClass + " intercambioBotonConCodigo"}
+          tabIndex={linkClass === "disabled" ? "-1" : "0"}
+          aria-label={
+            linkClass === "enabled" ? "Botón para intercambiar con código" : ""
+          }
+        >
+          <h2>Tengo código de intercambio</h2>
         </Link>
       </div>
     </>
