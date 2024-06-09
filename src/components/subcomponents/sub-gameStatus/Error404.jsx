f5f00851add981f4a804fd6dfbd2e68a669a1860
andresruiiz <andresruiz@uma.es>
2024-06-09 17:06:53 +0200

    feat: error 404 page

--- src/components/subcomponents/sub-gameStatus/Error404.jsx
+++ src/components/subcomponents/sub-gameStatus/Error404.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './styles/error404.css';
+import { Link } from 'react-router-dom';
 
 function Error404() {
     return (
-        <div>
+        <div id='errorContainer'>
+            <h1>404</h1>
             <h2>La página que estás buscando no existe</h2>
+            <Link to='/ruleta'>
+                <button className='error404Button'>Volver a la ruleta</button>
+            </Link>
         </div>
     )
 }
